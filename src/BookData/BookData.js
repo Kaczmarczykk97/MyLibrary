ffdb481ba6c2f528cbaeaebe4227697c9f23fa1a
Kaczmarczykk97 <kamil.kaczmarczykk@gmail.com>
2024-02-16 10:08:56 +0100

    add function to add rating stars

--- src/BookData/BookData.js
+++ src/BookData/BookData.js
@@ -14,6 +14,23 @@ function BookData() {
   const [startReadingData, setStartReadingData] = useState("");
   const [endReadingData, setEndReadingData] = useState("");
 
+  function setRatingStarsHandler(e) {
+    const starNr = e.target.parentNode.getAttribute("data-rating");
+    const allStars = e.target.closest("div");
+
+    if (starNr) {
+      const allStarsArray = Array.from(allStars.children);
+
+      allStarsArray.forEach((element) => {
+        element.style = "rgb(248, 248, 134)";
+
+        if (element.getAttribute("data-rating") <= starNr) {
+          element.style.color = "yellow";
+        }
+      });
+    }
+  }
+
   function setBooksDataHandler(e) {
     const inputID = e.target.parentNode.getAttribute("data-set");
     if (inputID === "hashtags") {
@@ -51,91 +68,103 @@ function BookData() {
       ></div>
       <div className={classes["bookData-text"]}>
         <h3>"Atomowe Nawyki"</h3>
-        <div className={classes["rating"]}>
-          <FaStar className={classes["icon-star"]} />
-          <FaStar className={classes["icon-star"]} />
-          <FaStar className={classes["icon-star"]} />
-          <FaStar className={classes["icon-star"]} />
-          <FaStar className={classes["icon-star"]} />
+        <div className={classes["rating"]} onClick={setRatingStarsHandler}>
+          <FaStar className={classes["icon-star"]} data-rating="1" />
+          <FaStar className={classes["icon-star"]} data-rating="2" />
+          <FaStar className={classes["icon-star"]} data-rating="3" />
+          <FaStar className={classes["icon-star"]} data-rating="4" />
+          <FaStar className={classes["icon-star"]} data-rating="5" />
+          <FaStar className={classes["icon-star"]} data-rating="6" />
         </div>
         <div className={classes["input-container"]} data-set="hashtags">
           <p>#Hashtags:</p>
           {hashtags && <p>{hashtags}</p>}
-          {!hashtags && <input type="text"></input>}
-          {!hashtags && (
-            <button
-              className={classes["bookData-btn"]}
-              onClick={setBooksDataHandler}
-            >
-              Add
-            </button>
-          )}
+          <div>
+            {!hashtags && <input type="text"></input>}
+            {!hashtags && (
+              <button
+                className={classes["bookData-btn"]}
+                onClick={setBooksDataHandler}
+              >
+                Add
+              </button>
+            )}
+          </div>
         </div>
         <div className={classes["input-container"]} data-set="author">
           <p>Autor:</p>
           {author && <p>{author}</p>}
-          {!author && <input type="text"></input>}
-          {!author && (
-            <button
-              className={classes["bookData-btn"]}
-              onClick={setBooksDataHandler}
-            >
-              Add
-            </button>
-          )}
+          <div>
+            {!author && <input type="text"></input>}
+            {!author && (
+              <button
+                className={classes["bookData-btn"]}
+                onClick={setBooksDataHandler}
+              >
+                Add
+              </button>
+            )}
+          </div>
         </div>
         <div className={classes["input-container"]} data-set="pagesAmount">
           <p>Liczba Stron:</p>
           {pagesAmount && <p>{pagesAmount}</p>}
-          {!pagesAmount && <input type="number"></input>}
-          {!pagesAmount && (
-            <button
-              className={classes["bookData-btn"]}
-              onClick={setBooksDataHandler}
-            >
-              Add
-            </button>
-          )}
+          <div>
+            {!pagesAmount && <input type="number"></input>}
+            {!pagesAmount && (
+              <button
+                className={classes["bookData-btn"]}
+                onClick={setBooksDataHandler}
+              >
+                Add
+              </button>
+            )}
+          </div>
         </div>
         <div className={classes["input-container"]} data-set="dataAdding">
           <p>Data dodania:</p>
           {dataAdding && <p>{dataAdding}</p>}
-          {!dataAdding && <input type="date"></input>}
-          {!dataAdding && (
-            <button
-              className={classes["bookData-btn"]}
-              onClick={setBooksDataHandler}
-            >
-              Add
-            </button>
-          )}
+          <div>
+            {!dataAdding && <input type="date"></input>}
+            {!dataAdding && (
+              <button
+                className={classes["bookData-btn"]}
+                onClick={setBooksDataHandler}
+              >
+                Add
+              </button>
+            )}
+          </div>
         </div>
-
         <div className={classes["input-container"]} data-set="startReadingData">
           <p>Start czytania:</p>
           {startReadingData && <p>{startReadingData}</p>}
-          {!startReadingData && <input type="date"></input>}
-          {!startReadingData && (
-            <button
-              className={classes["bookData-btn"]}
-              onClick={setBooksDataHandler}
-            >
-              Add
-            </button>
-          )}
+          <div>
+            {!startReadingData && <input type="date"></input>}
+            {!startReadingData && (
+              <button
+                className={classes["bookData-btn"]}
+                onClick={setBooksDataHandler}
+              >
+                Add
+              </button>
+            )}
+          </div>
         </div>
         <div className={classes["input-container"]} data-set="endReadingData">
           <p>Koniec czytania:</p>
           {endReadingData && <p>{endReadingData}</p>}
-          {!endReadingData && <input type="date"></input>}
-          {!endReadingData && (
-            <button
-              className={classes["bookData-btn"]}
-              onClick={setBooksDataHandler}
-            >
-              Add
-            </button>
-          )}
+          <div>
+            {!endReadingData && <input type="date"></input>}
+            {!endReadingData && (
+              <button
+                className={classes["bookData-btn"]}
+                onClick={setBooksDataHandler}
+              >
+                Add
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
